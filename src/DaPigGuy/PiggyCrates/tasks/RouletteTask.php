1d824fc244508a4898b1638dbc8de91f8b032f6d
Aericio <16523741+Aericio@users.noreply.github.com>
2020-03-29 19:29:16 -1000

    New Crate previews
    Ordered by percentage, maximum of 54 items can be shown in preview.

--- src/DaPigGuy/PiggyCrates/tasks/RouletteTask.php
+++ src/DaPigGuy/PiggyCrates/tasks/RouletteTask.php
@@ -7,11 +7,7 @@ namespace DaPigGuy\PiggyCrates\tasks;
 use DaPigGuy\PiggyCrates\crates\CrateItem;
 use DaPigGuy\PiggyCrates\PiggyCrates;
 use DaPigGuy\PiggyCrates\tiles\CrateTile;
-use muqsit\invmenu\inventory\InvMenuInventory;
-use muqsit\invmenu\InvMenu;
-use muqsit\invmenu\SharedInvMenu;
 use pocketmine\command\ConsoleCommandSender;
-use pocketmine\item\Item;
 use pocketmine\Player;
 use pocketmine\scheduler\Task;
 
@@ -30,34 +26,14 @@ class RouletteTask extends Task
     /** @var CrateItem[] */
     private $lastRewards = [];
 
-    /** @var bool */
-    private $preview = false;
-
-    /** @var SharedInvMenu */
-    private $menu;
-
-    /** @var Player|null */
-    private $player;
-
-    public function __construct(CrateTile $tile, ?Player $player = null, bool $preview = false)
+    public function __construct(CrateTile $tile)
     {
         $this->tile = $tile;
         $this->itemsLeft = $tile->getCrateType() === null ? 0 : $tile->getCrateType()->getDropCount();
-
-        if ($preview) {
-            $this->preview = true;
-            $this->player = $player;
-            $this->setupPreviewMenu();
-        }
     }
 
     public function onRun(int $currentTick): void
     {
-        if ($this->preview) {
-            $this->roulette();
-            return;
-        }
-
         if (!$this->tile->getCurrentPlayer() instanceof Player || !$this->tile->getCurrentPlayer()->isOnline() || ($crateType = $this->tile->getCrateType()) === null) {
             $this->tile->closeCrate();
             if (($handler = $this->getHandler()) !== null) $handler->cancel();
@@ -78,9 +54,7 @@ class RouletteTask extends Task
                     foreach ($crateType->getCommands() as $command) {
                         $this->tile->getCurrentPlayer()->getServer()->dispatchCommand(new ConsoleCommandSender(), str_replace("{PLAYER}", $this->tile->getCurrentPlayer()->getName(), $command));
                     }
-
                     $this->tile->getCurrentPlayer()->removeWindow($this->tile->getInventory());
-
                     $this->tile->closeCrate();
                     if (($handler = $this->getHandler()) !== null) $handler->cancel();
                 } else {
@@ -90,20 +64,6 @@ class RouletteTask extends Task
             }
             return;
         }
-        $this->roulette();
-    }
-
-    public function roulette(): void
-    {
-        $crateType = $this->tile->getCrateType();
-        if ($crateType === null) {
-            if (($handler = $this->getHandler()) !== null) {
-                $handler->cancel();
-            }
-            return;
-        }
-
-        if ($this->preview) $this->currentTick++;
 
         if ($this->currentTick % PiggyCrates::$instance->getConfig()->getNested("crates.roulette.speed") === 0) {
             $lastRewards = [];
@@ -114,41 +74,12 @@ class RouletteTask extends Task
             foreach ($this->lastRewards as $slot => $lastReward) {
                 if ($slot !== 9) {
                     $lastRewards[$slot - 1] = $lastReward;
-                    if ($this->preview) {
-                        $this->menu->getInventory()->setItem($slot - 1, $lastReward->getItem());
-                    } else {
-                        $this->tile->getInventory()->setItem($slot - 1, $lastReward->getItem());
-                    }
+                    $this->tile->getInventory()->setItem($slot - 1, $lastReward->getItem());
                 }
             }
-
             $lastRewards[17] = $crateType->getDrop(1)[0];
-            if ($this->preview) {
-                $this->menu->getInventory()->setItem(17, $lastRewards[17]->getItem());
-            } else {
-                $this->tile->getInventory()->setItem(17, $lastRewards[17]->getItem());
-            }
+            $this->tile->getInventory()->setItem(17, $lastRewards[17]->getItem());
             $this->lastRewards = $lastRewards;
         }
-    }
-
-    private function setupPreviewMenu(): void
-    {
-        $this->menu = InvMenu::create(InvMenu::TYPE_CHEST);
-        $this->menu->getInventory()->setItem(4, Item::get(Item::END_ROD, 0, 1));
-        $this->menu->getInventory()->setItem(22, Item::get(Item::END_ROD, 0, 1));
-        $this->menu->readonly();
-        $this->menu->setName($this->tile->getName());
-
-        if ($this->player instanceof Player && $this->player->isOnline()) {
-            $this->menu->send($this->player);
-        }
-
-        $this->menu->setInventoryCloseListener(function (Player $player, InvMenuInventory $inventory): bool {
-            if (($handler = $this->getHandler()) !== null) {
-                $handler->cancel();
-            }
-            return true;
-        });
     }
 }
--- src/DaPigGuy/PiggyCrates/tiles/CrateTile.php
+++ src/DaPigGuy/PiggyCrates/tiles/CrateTile.php
@@ -5,10 +5,14 @@ declare(strict_types=1);
 namespace DaPigGuy\PiggyCrates\tiles;
 
 use DaPigGuy\PiggyCrates\crates\Crate;
+use DaPigGuy\PiggyCrates\crates\CrateItem;
 use DaPigGuy\PiggyCrates\PiggyCrates;
 use DaPigGuy\PiggyCrates\tasks\RouletteTask;
+use muqsit\invmenu\InvMenu;
+use muqsit\invmenu\SharedInvMenu;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\item\Item;
+use pocketmine\level\Level;
 use pocketmine\level\particle\FloatingTextParticle;
 use pocketmine\nbt\tag\CompoundTag;
 use pocketmine\network\mcpe\protocol\BlockEventPacket;
@@ -30,6 +34,17 @@ class CrateTile extends Chest
 
     /** @var array[] */
     public $floatingTextParticles = [];
+
+    /** @var SharedInvMenu */
+    private $menu;
+
+    public function __construct(Level $level, CompoundTag $nbt)
+    {
+        parent::__construct($level, $nbt);
+        $this->menu = InvMenu::create($this->crateType->dropCount > 9 ? InvMenu::TYPE_DOUBLE_CHEST : InvMenu::TYPE_CHEST);
+        $this->menu->readonly();
+        $this->menu->setName($this->crateType->getName() . " Crate");
+    }
 
     public function getCrateType(): ?Crate
     {
@@ -105,7 +120,25 @@ class CrateTile extends Chest
     {
         if (($crateType = $this->crateType) === null || ($level = $this->getLevel()) === null) return;
 
-        PiggyCrates::$instance->getScheduler()->scheduleRepeatingTask(new RouletteTask($this, $player, true), 1);
+        $chances = 0;
+        foreach ($this->crateType->drops as $crateItem) $chances += $crateItem->chance;
+
+        usort($this->crateType->drops, function (CrateItem $a, CrateItem $b) {
+            if ($a->getChance() > $b->getChance()) return -1;
+            if ($a->getChance() < $b->getChance()) return 1;
+            return 0;
+        });
+
+        $slot = 0;
+        foreach ($this->crateType->drops as $crateItem) {
+            if ($slot >= 53) return; // Maximum supported preview items is 54, meaning lowest chances are not shown.
+            $item = clone $crateItem->item;
+            $item->setCustomName(TextFormat::RESET . TextFormat::GREEN . $crateItem->getItem()->getCount() . "x " . TextFormat::RESET . $item->getName());
+            $item->setLore([TextFormat::RESET, TextFormat::RESET . TextFormat::GOLD . "Chance: " . round(($crateItem->chance / $chances) * 100, 2, PHP_ROUND_HALF_UP) . "%"]);
+            $this->menu->getInventory()->setItem($slot, $item);
+            $slot++;
+        }
+        $this->menu->send($player);
     }
 
     public function close(): void
